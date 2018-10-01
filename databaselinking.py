799e5b2d6e935d4e304cabfbf8c5f991662b5c8a
VARUN KUMAR <33143847+varun1945@users.noreply.github.com>
2018-10-01 17:48:12 +0530

    added operations on table
    insertions

--- databaselinking.py
+++ databaselinking.py
@@ -1,11 +1,11 @@
 import sqlite3
 db=sqlite3.connect('student1.db')
 c=db.cursor()
-#c.execute("insert into data4 values('vinit',61,'vinit2gmail.com')")
-#c.execute("insert into data4 values('yash',64,'yash@gmail.com')")
-#c.execute("delete from data4 where roll=64")
-#c.execute("insert into data4 values('xyz',100,'xyz@gmail.com')")
-#c.execute("select * from data4")
+c.execute("insert into data4 values('vinit',61,'vinit2gmail.com')")
+c.execute("insert into data4 values('yash',64,'yash@gmail.com')")
+c.execute("delete from data4 where roll=64")
+c.execute("insert into data4 values('xyz',100,'xyz@gmail.com')")
+c.execute("select * from data4")
 """r=c.fetchall()
 for i in r:
     print(i)"""
